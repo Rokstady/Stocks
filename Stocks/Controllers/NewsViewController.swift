93f76be26792dfab0f11c6ca998e39228748d717
Yaroslav Yakovlev <jaroslawjakowlew@gmail.com>
2022-02-03 19:34:50 +0100

    Opening news

--- Stocks/Controllers/NewsViewController.swift
+++ Stocks/Controllers/NewsViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SafariServices
 
 class NewsViewController: UIViewController {
 
@@ -91,7 +92,8 @@ class NewsViewController: UIViewController {
     }
     
     private func open(url:URL) {
-        
+        let vc = SFSafariViewController(url: url)
+        present(vc,animated: true)
     }
 }
 
@@ -134,6 +136,23 @@ extension NewsViewController: UITableViewDelegate,UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+        
+        let story = stories[indexPath.row]
+        guard let url = URL(string: story.url) else {
+            presentFailedToOpenAlert()
+            return
+        }
+        open(url: url)
+    }
+    
+    private func presentFailedToOpenAlert() {
+        let alert = UIAlertController(
+            title: "Unable to Open",
+            message: "We unable to open the article",
+            preferredStyle: .alert
+        )
+        alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
+        present(alert, animated: true)
     }
     
 }
